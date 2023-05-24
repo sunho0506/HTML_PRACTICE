6d7ceeee76d8e9393a2504fc290dc4c44d33e939
ykkc21 <ykkc1595@gmail.com>
2023-05-24 13:38:01 +0900

    ㅋ

--- js/app.js
+++ js/app.js
@@ -1,14 +1,9 @@
 $(() => {
-  let check = false;
-
   $(".click_button").click(() => {
-    check = true;
-    if (check == true) {
-      $(".chat_box").dialog({
-        width: 900,
-        height: 650,
-        title: "InHaChat",
-      });
-    }
+    $(".chat_box").dialog({
+      width: 900,
+      height: 650,
+      title: "InHaChat",
+    });
   });
 });
